47e57e2ece4fbf393a2cb9150dd3fb43a06a7ff2
Vitalina <litilian555@gmail.com>
2023-12-21 19:30:19 +0700

    Button click event

--- Calculator.js
+++ Calculator.js
@@ -33,3 +33,23 @@ function changeSign() {
     expression = eval(expression * -1).toString();
     updateDisplay();
 }
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const value = button.getAttribute('data-value');
+      if (!isNaN(parseFloat(value)) || value === '.') {
+        inputNumber(value);
+      } else if (value === '+' || value === '-' || value === '*' || value === '/') {
+        inputOperator(value);
+      } else if (value === '=') {
+        calculate();
+      } else if (value === 'sign') {
+        changeSign();
+      } else if (value === 'clear') {
+        expression = '';
+        result = null;
+        updateDisplay();
+      }
+    });
+});
+  
